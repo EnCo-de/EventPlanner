f7c4d5f53f0de5041d9cd3bbfce5b2f3495e035e
Event Planner <142403662+EnCo-de@users.noreply.github.com>
2023-12-31 13:20:53 +0400

    Create a Build Script Go Production-Ready

--- EventPlanner/settings.py
+++ EventPlanner/settings.py
@@ -98,7 +98,8 @@ else:
     # Following settings only make sense on production and may break development environments.
     # Tell Django to copy statics to the `staticfiles` directory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = BASE_DIR / "staticfiles"
 
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
@@ -107,6 +108,7 @@ else:
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'customauth.apps.CustomauthConfig',
     'django.contrib.admin',
     'django.contrib.auth',
